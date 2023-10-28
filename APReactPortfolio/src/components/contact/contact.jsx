43bec7120cb14af2ba23f892f812fc8d5c0bd1fe
adrummer1 <andrew@bluestemcreativeco.com>
2023-10-28 13:09:42 -0500

    Added form field requirement notifications

--- APReactPortfolio/src/components/contact/contact.jsx
+++ APReactPortfolio/src/components/contact/contact.jsx
@@ -5,8 +5,11 @@ import { validateEmail } from '../../utils/helpers';
 function Contact() {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMessage] =useState('');
+  const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [userNameTouched, setUserNameTouched] = useState(false);
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [messageTouched, setMessageTouched] = useState(false);
 
   const handleInputChange = (e) => {
     const { target } = e;
@@ -15,10 +18,13 @@ function Contact() {
 
     if (inputType === 'userName') {
       setUserName(inputValue);
+      setUserNameTouched(true);
     } else if (inputType === 'email') {
       setEmail(inputValue);
+      setEmailTouched(true);
     } else if (inputType === 'message') {
-        setMessage(inputValue);
+      setMessage(inputValue);
+      setMessageTouched(true);
       }
     };
 
@@ -38,12 +44,13 @@ function Contact() {
 
     return (
       <div className="container text-center">
-      <h1 id="contact" className="form-title">Contact Me {userName}</h1>
+      <h1 id="contact" className="form-title">Contact Me</h1>
       <form className="form" onSubmit={handleFormSubmit}>
         <input
           value={userName}
           name="userName"
           onChange={handleInputChange}
+          onBlur={() => setUserNameTouched(true)}
           type="text"
           placeholder="username"
         />
@@ -51,6 +58,7 @@ function Contact() {
           value={email}
           name="email"
           onChange={handleInputChange}
+          onBlur={() => setEmailTouched(true)}
           type="email"
           placeholder="email"
         />
@@ -58,9 +66,19 @@ function Contact() {
           value={message}
           name="message"
           onChange={handleInputChange}
+          onBlur={() => setMessageTouched(true)}
           type="text"
           placeholder="message"
         />
+        {userNameTouched && !userName && (
+          <p className="required-field-notification">Username is required</p>
+        )}
+        {emailTouched && !email && (
+          <p className="required-field-notification">Email is required</p>
+        )}
+        {messageTouched && !message && (
+          <p className="required-field-notification">Message is required</p>
+        )}
         <button className="submit" type="submit">
           Submit
         </button>
