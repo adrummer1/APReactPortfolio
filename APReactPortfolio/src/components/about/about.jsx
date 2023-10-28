687da708df2816b2d4e4a91fc860f749b048af23
adrummer1 <andrew@bluestemcreativeco.com>
2023-10-28 13:15:29 -0500

    Updated About section

--- APReactPortfolio/src/components/about/about.jsx
+++ APReactPortfolio/src/components/about/about.jsx
@@ -9,8 +9,8 @@ const About = () => {
     <section id="about">
       <div className="aboutContent">
         <span className="intro">Hi!</span>
-        <span className="introText">My name is <span className="introName">Andrew</span> <br />and I am a <span className="title">Web Developer</span></span>
-        <p className="introGraph">I have 20+ years experience in the publishing industry with a focus on marketing, UX/UI design, and ecommerce. Keep scrolling to check out some of my work!</p>
+        <span className="introText">My name is <span className="introName">Andrew</span> <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and I am a <span className="title">Web Developer</span></span>
+        <p className="introGraph">I have 20+ years experience in the media industry with a focus on marketing, UX/UI design, and ecommerce. Keep scrolling to check out some of my work!</p>
       </div>
       <img src={me} alt="profile" className="me" />
     </section>
