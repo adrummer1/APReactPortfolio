016d2d016d1175c4621b989a29817bb1f90a3c90
adrummer1 <andrew@bluestemcreativeco.com>
2023-11-13 16:02:48 -0600

    Updated contact in nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -16,11 +16,12 @@ const Navbar = () => {
         <Link activeClass='active' to='/about' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">About</Link>
         <Link activeClass='active' to='/projects' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Portfolio</Link>
         <Link activeClass='active' to='/resume' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Resume</Link>
+        <Link activeClass='active' to='/contact' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Contact</Link>
       </div>
-      <button className="desktopMenuBtn" onClick={() => {
+      {/* <button className="desktopMenuBtn" onClick={() => {
         window.location.href = "/contact"
       }}>
-        <img src="" alt="" className="desktopMenuImg" />Contact Me</button>
+        <img src="" alt="" className="desktopMenuImg" />Contact Me</button> */}
 
         <img src={hamenu} alt="Menu" className='hamenu' onClick={()=>setShowMenu(!showMenu)}/>
         <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
